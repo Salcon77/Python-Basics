211a86d37fbfb24c1507835b1803cf0a07ae5bfd
Salcon77 <Salcon77@users.noreply.github.com>
2018-08-11 20:29:12 -0500

    If/else and For loop
    Added files on if else statments and for loops

---
+++ For loop.py
@@ -0,0 +1,4 @@
+"""
+The for loop in Python is used to iterate over a sequence (list, tuple, string) or other iterable objects.
+Iterating over a sequence is called traversal.
+"""
---
+++ If else.py
@@ -0,0 +1,5 @@
+"""
+Decision making is required when we want to execute a code only if a certain condition is satisfied.
+
+The if…elif…else statement is used in Python for decision making.
+"""
