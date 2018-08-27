1b12aa246a09cc48d46952831348d388a8d9fe46
Salcon77 <Salcon77@users.noreply.github.com>
2018-08-26 20:37:36 -0500

    Numbers

---
+++ Data Types/Numbers.py
@@ -0,0 +1,17 @@
+#NUMBERS
+
+#Python supports integers, floating point numbers and complex numbers. They are defined as int, float and
+# complex class in Python.
+a = 5
+print(type(a))
+b =5.0
+print(type(b))
+c = 5 + 3j
+print(type(c+3))
+
+#Integers and floating points are separated by the presence or absence of a decimal point. 5 is integer
+# whereas 5.0 is a floating point number.
+# While integers can be of any length, a floating point number is accurate only up to 15 decimal places
+# (the 16th place is inaccurate).
+
+#Complex numbers are written in the form, x + yj, where x is the real part and y is the imaginary part.
