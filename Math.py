97b494307391ab529e2219ee17341c5b236f6af6
Salcon77 <Salcon77@users.noreply.github.com>
2018-08-28 21:28:13 -0500

    pi

--- Math.py
+++ Math.py
@@ -6,3 +6,16 @@ print(math.pi)
 
 print(math.cos(math.pi))
 
+print(math.cos(math.pi))
+
+# Output: 22026.465794806718
+print(math.exp(10))
+
+# Output: 3.0
+print(math.log10(1000))
+
+# Output: 1.1752011936438014
+print(math.sinh(1))
+
+# Output: 720
+print(math.factorial(6))
