9f68470b956acdc9d5114b36f36470f71032974e
Salcon77 <Salcon77@users.noreply.github.com>
2018-08-30 23:00:35 -0500

    lists

---
+++ Data Types/Lists.py
@@ -0,0 +1,34 @@
+# empty list
+my_list = []
+
+# list of integers
+my_list = [1, 2, 3]
+
+# list with mixed datatypes
+my_list = [1, "Hello", 3.4]
+
+
+
+my_list = ['p','r','o','b','e']
+# Output: p
+print(my_list[0])
+
+# Output: o
+print(my_list[2])
+
+# Output: e
+print(my_list[4])
+
+# Error! Only integer can be used for indexing
+# my_list[4.0]
+
+# Nested List
+n_list = ["Happy", [2,0,1,5]]
+
+# Nested indexing
+
+# Output: a
+print(n_list[0][1])
+
+# Output: 5
+print(n_list[1][3])
