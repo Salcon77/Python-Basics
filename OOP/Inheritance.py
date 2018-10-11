90aceac2c67644db3cc5639547cd36eb7fa3e262
Salcon77 <Salcon77@users.noreply.github.com>
2018-10-10 20:05:11 -0500

    Inheritence

---
+++ OOP/Inheritance.py
@@ -0,0 +1,4 @@
+#INHERITENCE
+
+# Inheritence to defining a new class with little or no modification to an existing class.
+# The new class is called derived ( child) class and the one from which it inherits is called the base(or parent) class.
