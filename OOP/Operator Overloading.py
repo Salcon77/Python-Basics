d49ca6bccbcaa4346d5e8fbcb726ebf3c8e727e1
Salcon77 <Salcon77@users.noreply.github.com>
2018-10-16 18:43:48 -0500

    OPerator Overloaidng

---
+++ OOP/Operator Overloading.py
@@ -0,0 +1,8 @@
+#Operator OverLoading
+
+"""
+Python operators work for built-in classes. But same operator behaves differently with different types.
+For example,the + operator will, perform arithmetic addition on 2 numbers, merge two lists and concatenate two strings.
+"""
+
+# Class functions that begins with double underscore __ are called special functions in Python
